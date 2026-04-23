ca7e94a1eb67b3b9bb3cd01c375ed8d11ab5ddc1
Васюкова Наталья Павловна <144936520+Vasyukova-Nat@users.noreply.github.com>
2026-04-23 21:37:25 +0300

    fixed static

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,10 @@ import QueryTable from "./components/QueryTable";
 function App() {
   return (
     <div>
-      <h1 style={{ padding: "20px 16px 0", fontSize: 24 }}>Поисковые запросы</h1>
+      <h1 style={{ padding: "20px 16px 0 16px", fontSize: 24 }}>Поисковые запросы</h1>
+      <p style={{ padding: "0 16px", color: "#000000", fontSize: 14, marginBottom: 0 }}>
+        Разработчик: Васюкова Наталья
+      </p>
       <QueryTable />
     </div>
   );
